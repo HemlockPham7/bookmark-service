74ff798ddf706f3a995ae7eef69f7d6bd95bef84
VVVietAnh <157110579+HemlockPham7@users.noreply.github.com>
2026-08-15 23:49:08 +0700

    main go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,21 @@
 package main
+
+import "github.com/HemlockPham7/bookmark-service/internal/infrastructure"
+
+// @title Bookmark Service API
+// @version 1.0.0
+// @description This is the API documentation for the Bookmark service.
+// @BasePath /
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+func main() {
+	// Init api
+	app := infrastructure.CreateAPI()
+
+	// Run api
+	err := app.Start()
+	if err != nil {
+		panic(err)
+	}
+}
