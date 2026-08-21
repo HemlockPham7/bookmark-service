cd509b045935e87b34ac321e4d57ca799007da7c
VVVietAnh <157110579+HemlockPham7@users.noreply.github.com>
2026-08-21 20:27:25 +0700

    integrate newrelic for bookmark-service

--- internal/app/repository/bookmark/get_bookmark.go
+++ internal/app/repository/bookmark/get_bookmark.go
@@ -5,9 +5,13 @@ import (
 
 	"github.com/HemlockPham7/bookmark-service/internal/app/model"
 	"github.com/HemlockPham7/common-libs/pkg/dbutils"
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 func (r *bookmarkRepository) GetBookmarks(ctx context.Context, userID string, limit, offset int) ([]*model.Bookmark, int64, error) {
+	span := newrelic.FromContext(ctx).StartSegment("GetBookmarks_BookmarkRepository")
+	defer span.End()
+
 	bookmarks, err := r.getBookmarks(ctx, userID, limit, offset)
 	if err != nil {
 		return nil, 0, err
@@ -22,6 +26,9 @@ func (r *bookmarkRepository) GetBookmarks(ctx context.Context, userID string, li
 }
 
 func (r *bookmarkRepository) getBookmarks(ctx context.Context, userID string, limit, offset int) ([]*model.Bookmark, error) {
+	span := newrelic.FromContext(ctx).StartSegment("GetBookmarksgetBookmarks_BookmarkRepository")
+	defer span.End()
+
 	bookmarks := make([]*model.Bookmark, 0, limit)
 
 	err := r.db.WithContext(ctx).Where("user_id = ?", userID).Order("created_at ASC").Offset(offset).Limit(limit).Find(&bookmarks).Error
@@ -33,6 +40,9 @@ func (r *bookmarkRepository) getBookmarks(ctx context.Context, userID string, li
 }
 
 func (r *bookmarkRepository) countBookmarks(ctx context.Context, userID string) (int64, error) {
+	span := newrelic.FromContext(ctx).StartSegment("GetBookmarkscountBookmarks_BookmarkRepository")
+	defer span.End()
+
 	var total int64
 
 	err := r.db.WithContext(ctx).Model(&model.Bookmark{}).Where("user_id = ?", userID).Count(&total).Error
